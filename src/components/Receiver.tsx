36f08dbdb52e34de17dbe655f9be552a58ca36e8
Joji Augustine <jojimail@gmail.com>
2021-04-13 04:40:32 +0530

    Retry and scan

--- src/components/Receiver.tsx
+++ src/components/Receiver.tsx
@@ -1,14 +1,18 @@
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useStore } from '../stores';
-import { Box, Button, Center } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, Link } from '@chakra-ui/react';
+import QrReader from 'react-qr-reader';
+import { useHistory, Link as RouterLink } from 'react-router-dom';
 
 const Receiver = observer(() => {
   const { remoteId }: { remoteId?: string } = useParams();
 
   const store = useStore();
   const { connect } = store;
+
+  const history = useHistory();
 
   useEffect(() => {
     if (remoteId && connect.id) {
@@ -22,20 +26,56 @@ const Receiver = observer(() => {
     connect.sendFileDownloadRequest();
   };
 
+  const handleQRScan = (data: string | null) => {
+    if (data) {
+      console.log('Qr data', data);
+      const path = data;
+      const remoteIdFromQr = path.substring(path.lastIndexOf('/') + 1).trim();
+      if (remoteIdFromQr.length > 0) {
+        console.log('Qr Remite', remoteIdFromQr);
+        history.push(`/receive/${remoteIdFromQr}`);
+      }
+    }
+  };
+
+  const handleQRError = (error: Error) => {
+    console.log('Error', error);
+  };
+
   return (
     <Box>
       <Center flexDir="column">
-        {!connect.fileMeta && <Box mb={5}>No File available</Box>}
-
-        {connect.fileMeta && (
+        {!remoteId && (
           <Box mb={5}>
-            File Selected
-            <Box>Filename : {connect.fileMeta.name}</Box>
-            <Box>Type : {connect.fileMeta.type}</Box>
-            <Box>Size : {connect.fileMeta.size}</Box>
+            <QrReader
+              delay={300}
+              onError={handleQRError}
+              onScan={handleQRScan}
+              style={{ width: '300px', height: '300px' }}
+            />
           </Box>
         )}
-        <Button onClick={handleDownloadClick}>Download</Button>
+
+        {remoteId && connect.fileMeta && (
+          <Flex mb={5} flexDirection="column">
+            <Box mb={5}>
+              File Selected
+              <Box>Filename : {connect.fileMeta.name}</Box>
+              <Box>Type : {connect.fileMeta.type}</Box>
+              <Box>Size : {connect.fileMeta.size}</Box>
+            </Box>
+            <Button onClick={handleDownloadClick}>Download</Button>
+          </Flex>
+        )}
+
+        {remoteId && !connect.fileMeta && (
+          <Flex mb={5} flexDirection="column" alignItems="center">
+            <Box>Could not get file</Box>
+            <Link as={RouterLink} to="/receive/" py={5}>
+              <Button>Retry</Button>
+            </Link>
+          </Flex>
+        )}
       </Center>
     </Box>
   );
